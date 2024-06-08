9f7ca9bfaebd497cb11ddde4e37a3a556a27b869
maulanaa121 <fm886211@gmail.com>
2024-06-08 22:48:02 +0700

    menambahkan animasi scroll

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const Menu_icon = document.querySelector('#menu-icon');
 const nav_list = document.querySelector('.nav-list');
 const nav = document.querySelector('nav');
-
+const sr = ScrollReveal({
+    distance:'60px',
+    duration:2500,
+    delay:400,
+    reset:true
+})
 
 // membuat menu toggle
 Menu_icon.addEventListener('click',()=>{
@@ -13,3 +18,8 @@ Menu_icon.addEventListener('click',()=>{
 window.addEventListener('scroll',()=>{
     nav.classList.toggle('sticky',window.scrollY>50)
 })
+
+// membuat animasi delay saat scroll
+sr.reveal('.home .home-img',{delay:800,origin:'right'})
+sr.reveal('.home .home-content',{delay:800,origin:'left'})
+sr.reveal('.home .home-content .wrapper',{delay:800,origin:'bottom'})
